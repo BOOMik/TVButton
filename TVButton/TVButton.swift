84f7bc650238d29f59af2cd43315dee0677a4107
Kirill Ashikhmin <ka@binwell.com>
2016-11-13 22:55:08 +0300

    0.1.7 Swift 3

--- TVButton/TVButton.swift
+++ TVButton/TVButton.swift
@@ -29,7 +29,7 @@ public extension TVButtonLayer {
 /**
  TVButton Object
  */
-public class TVButton: UIButton, UIGestureRecognizerDelegate {
+open class TVButton: UIButton, UIGestureRecognizerDelegate {
     
     // MARK: Internal variables
     internal var containerView = UIView()
@@ -43,7 +43,7 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
     // MARK: Public variables
     
     /// Stack of TVButtonLayers inside the button
-    public var layers: [TVButtonLayer]? {
+    open var layers: [TVButtonLayer]? {
         didSet {
             // Remove existing parallax layer views
             for subview in containerView.subviews {
@@ -57,7 +57,7 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
                     imageView.clipsToBounds = true
                     imageView.layer.needsDisplayOnBoundsChange = true
                     if saveAspect {
-                        imageView.contentMode = .ScaleAspectFill
+                        imageView.contentMode = .scaleAspectFill
                     }
                     containerView.addSubview(imageView)
                 }
@@ -65,8 +65,8 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
                 if let specularImage = customSpecularImage {
                     specularView.image = specularImage
                 } else {
-                    let frameworkBundle = NSBundle(forClass: TVButton.self)
-                    let specularViewPath = frameworkBundle.pathForResource("Specular", ofType: "png")
+                    let frameworkBundle = Bundle(for: TVButton.self)
+                    let specularViewPath = frameworkBundle.path(forResource: "Specular", ofType: "png")
                     specularView.image = UIImage(contentsOfFile: specularViewPath!)
                 }
                 updateFrame()
@@ -75,17 +75,17 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
         }
     }
     /// True if need save image aspect
-    public var customSpecularImage: UIImage?
+    open var customSpecularImage: UIImage?
     /// True if need save image aspect
-    public var saveAspect: Bool = defaultSaveAspect
+    open var saveAspect: Bool = defaultSaveAspect
 
     /// Determines the intensity of the parallax depth effect. Default is 1.0.
-    public var parallaxIntensity: CGFloat = defaultParallaxIntensity
+    open var parallaxIntensity: CGFloat = defaultParallaxIntensity
 
     /// Shadow color for the TVButton. Default is black.
-    public var shadowColor: UIColor = UIColor.blackColor() {
+    open var shadowColor: UIColor = UIColor.black {
         didSet {
-            self.layer.shadowColor = shadowColor.CGColor
+            self.layer.shadowColor = shadowColor.cgColor
         }
     }
     
@@ -110,13 +110,13 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
     /**
      Lays out subviews.
      */
-    override public func layoutSubviews() {
+    override open func layoutSubviews() {
         super.layoutSubviews()
         containerView.frame = self.bounds
         self.layer.masksToBounds = false;
         if (containerView.subviews.count > 0) {
             let shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: cornerRadius)
-            self.layer.shadowPath = shadowPath.CGPath
+            self.layer.shadowPath = shadowPath.cgPath
         } else {
             self.layer.shadowPath = nil
         }
@@ -130,7 +130,7 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
         // Adjust size for every subview
         for subview in containerView.subviews {
             if subview == specularView {
-                subview.frame = CGRect(origin: subview.frame.origin, size: CGSizeMake(specularScale * containerView.frame.size.width, specularScale * containerView.frame.size.height))
+                subview.frame = CGRect(origin: subview.frame.origin, size: CGSize(width: specularScale * containerView.frame.size.width, height: specularScale * containerView.frame.size.height))
             }
             else {
                 subview.frame = CGRect(origin: subview.frame.origin, size: containerView.frame.size)
@@ -143,15 +143,15 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
      Button setup. Conducted on init.
     */
     func setup() {
-        containerView.userInteractionEnabled = false
+        containerView.isUserInteractionEnabled = false
         self.addSubview(containerView)
         containerView.clipsToBounds = true
         containerView.layer.cornerRadius = cornerRadius
         self.clipsToBounds = true
         specularView.alpha = 0.0
-        specularView.contentMode = UIViewContentMode.ScaleAspectFill
+        specularView.contentMode = UIViewContentMode.scaleAspectFill
         self.layer.shadowRadius = self.bounds.size.height/(2*shadowFactor)
-        self.layer.shadowOffset = CGSizeMake(0.0, shadowFactor/3)
+        self.layer.shadowOffset = CGSize(width: 0.0, height: shadowFactor/3)
         self.layer.shadowOpacity = 0.5;
         tvButtonAnimation = TVButtonAnimation(button: self)
         self.addGestureRecognizers()
@@ -164,12 +164,12 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
     Adds the gesture recognizers to the button.
     */
     func addGestureRecognizers(){
-        panGestureRecognizer = UIPanGestureRecognizer(target: self, action: "handlePan:")
+        panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(TVButton.handlePan(_:)))
         panGestureRecognizer?.delegate = self
         self.addGestureRecognizer(panGestureRecognizer!)
-        tapGestureRecognizer = UITapGestureRecognizer(target: self, action: "handleTap:")
+        tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(TVButton.handleTap(_:)))
         self.addGestureRecognizer(tapGestureRecognizer!)
-        longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: "handleLongPress:")
+        longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(TVButton.handleLongPress(_:)))
         longPressGestureRecognizer?.delegate = self
         self.addGestureRecognizer(longPressGestureRecognizer!)
     }
@@ -178,7 +178,7 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
      Pan gesture recognizer handler.
      - Parameter gestureRecognizer: TVButton's UIPanGestureRecognizer.
      */
-    func handlePan(gestureRecognizer: UIGestureRecognizer) {
+    func handlePan(_ gestureRecognizer: UIGestureRecognizer) {
         self.gestureRecognizerDidUpdate(gestureRecognizer)
     }
     
@@ -186,7 +186,7 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
      Long press gesture recognizer handler.
      - Parameter gestureRecognizer: TVButton's UILongPressGestureRecognizer.
      */
-    func handleLongPress(gestureRecognizer: UIGestureRecognizer) {
+    func handleLongPress(_ gestureRecognizer: UIGestureRecognizer) {
         self.gestureRecognizerDidUpdate(gestureRecognizer)
     }
     
@@ -194,29 +194,29 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
      Tap gesture recognizer handler. Sends TouchUpInside to super.
      - Parameter gestureRecognizer: TVButton's UITapGestureRecognizer.
      */
-    func handleTap(gestureRecognizer: UIGestureRecognizer) {
-        super.sendActionsForControlEvents(UIControlEvents.TouchUpInside)
+    func handleTap(_ gestureRecognizer: UIGestureRecognizer) {
+        super.sendActions(for: UIControlEvents.touchUpInside)
     }
     
     /**
      Determines button's reaction to gesturerecognizer.
      - Parameter gestureRecognizer: either UITapGestureRecognizer or UILongPressGestureRecognizer.
      */
-    func gestureRecognizerDidUpdate(gestureRecognizer: UIGestureRecognizer){
+    func gestureRecognizerDidUpdate(_ gestureRecognizer: UIGestureRecognizer){
         if layers == nil {
             return
         }
-        let point = gestureRecognizer.locationInView(self)
+        let point = gestureRecognizer.location(in: self)
         if let animation = tvButtonAnimation {
-            if gestureRecognizer.state == .Began {
+            if gestureRecognizer.state == .began {
                 animation.enterMovement()
                 animation.processMovement(point)
             }
-            else if gestureRecognizer.state == .Changed {
+            else if gestureRecognizer.state == .changed {
                 animation.processMovement(point)
             }
             else {
-                if gestureRecognizer.state == .Began || gestureRecognizer.state == .Changed {
+                if gestureRecognizer.state == .began || gestureRecognizer.state == .changed {
                     return
                 }
                 animation.exitMovement()
@@ -231,24 +231,24 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
     - Parameter gestureRecognizer: First gestureRecognizer.
     - Parameter otherGestureRecognizer: Second gestureRecognizer.
     */
-    public func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+    open func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
         return true
     }
 
 
-    func getScaledSizeOfImage(image: UIImage, toSize: CGSize) -> CGSize
+    func getScaledSizeOfImage(_ image: UIImage, toSize: CGSize) -> CGSize
     {
         let widthRatio = toSize.width/image.size.width
         let heightRatio = toSize.height/image.size.height
         let scale = min(widthRatio, heightRatio)
         let imageWidth = scale*image.size.width
         let imageHeight = scale*image.size.height
-        return CGSizeMake(imageWidth, imageHeight)
+        return CGSize(width: imageWidth, height: imageHeight)
     }
 
 
 
-    public func updateFrame() {
+    open func updateFrame() {
         if (saveAspect && layers != nil && layers!.count > 0) {
             var newSize:CGSize = getScaledSizeOfImage(layers![0].internalImage!, toSize: self.frame.size)
 
